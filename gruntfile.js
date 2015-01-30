744472d8f7e737ae3e04511302a34d15ac38004c
Ciro Cesar Maciel <lilo@c37.co>
2015-01-30 16:17:07 -0200

    initial commit :-)

---
+++ gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function (grunt) {
+
+    // Load the plugins that provides tasks
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            dist: '<%= pkg.directories.dist %>',
+            doc: '<%= pkg.directories.doc %>',
+            src: '<%= pkg.directories.src %>'
+        },
+        meta: {
+            banner: '/*!\n' +
+                ' * C37 in <%= grunt.template.today("dd-mm-yyyy") %> at <%= grunt.template.today("HH:MM:ss") %> \n' +
+                ' *\n' +
+                ' * <%= pkg.name %> version: <%= pkg.version %>\n' +
+                ' * licensed by Creative Commons Attribution-ShareAlike 3.0\n' +
+                ' *\n' +
+                ' * Copyright - C37 - http://ww.c37.co - <%= grunt.template.today("yyyy") %>\n' +
+                ' */\n',
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: ['pkg'],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
+        },
+        watch: {
+            src: {
+                files: ['<%= dirs.src %>/**/*.less'],
+                tasks: ['default']
+            }
+        },
+        less: {
+            options: {
+                banner: '<%= meta.banner %> \n'
+            },
+            development: {
+                options: {
+                    compress: false
+                },
+                files: {
+                    '<%= dirs.dist %>/style-<%= pkg.version %>.css': '<%= dirs.src %>/style.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    '<%= dirs.dist %>/style-<%= pkg.version %>.css': '<%= dirs.src %>/style.less'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9'],
+                diff: '<%= dirs.dist %>/style-<%= pkg.version %>.css.diff'
+            },
+            files: {
+                src: '<%= dirs.dist %>/style-<%= pkg.version %>.css',
+                dest: '<%= dirs.dist %>/style-<%= pkg.version %>.css'
+            },
+        }
+
+
+    });
+
+    // Events
+    grunt.event.on('watch', function (action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
+
+    // Tasks
+    grunt.registerTask('dist', ['bump', 'less:production', 'autoprefixer']);
+    grunt.registerTask('default', ['less:development', 'autoprefixer']);
+};
