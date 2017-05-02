3874ff1957240ffc26a3e069b9e3f3ddae35d0de
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-02 20:33:03 -0300

    - nav-menu

--- docs/assets/js/library/header.js
+++ docs/assets/js/library/header.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 02 2017 20:20:59 GMT-0300 (BRT)
+ * Static Here - in Tue May 02 2017 20:32:40 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
