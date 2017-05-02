245182d86ad77088eebd18034a371964bdb81f9e
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-02 11:40:27 -0300

    - components para responsive

--- docs/assets/js/library/form.js
+++ docs/assets/js/library/form.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 02 2017 11:36:52 GMT-0300 (BRT)
+ * Static Here - in Tue May 02 2017 11:40:14 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
