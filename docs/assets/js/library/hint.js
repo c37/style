d8876d4903632129ae23bf8ec926434019c6144b
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-02 11:37:05 -0300

    - mais para responsive

--- docs/assets/js/library/hint.js
+++ docs/assets/js/library/hint.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 02 2017 11:31:00 GMT-0300 (BRT)
+ * Static Here - in Tue May 02 2017 11:36:53 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
