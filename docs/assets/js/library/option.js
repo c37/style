e116341e3aae4ba383f3a0fbc3ce9a8e9952aa3a
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-09 21:09:12 -0300

    - about us

--- docs/assets/js/library/option.js
+++ docs/assets/js/library/option.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 09 2017 20:55:02 GMT-0300 (BRT)
+ * Static Here - in Tue May 09 2017 21:08:05 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
--- docs/assets/js/vendor/i18next.js
+++ docs/assets/js/vendor/i18next.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 09 2017 20:55:05 GMT-0300 (BRT)
+ * Static Here - in Tue May 09 2017 21:08:08 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
