026f8b711ababd33f38ce38c1d4beef505a4c88e
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-10 13:15:24 -0300

    - form + modal  - revisoes em width

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Wed May 10 2017 12:31:13 GMT-0300 (BRT)
+ * Static Here - in Wed May 10 2017 13:15:04 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
