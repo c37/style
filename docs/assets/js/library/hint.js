d2ea0aacbfd43ac965ca83c70566a8551f39acf1
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-12 14:55:32 -0300

    - icons - font-size - heading padding

--- docs/assets/js/library/hint.js
+++ docs/assets/js/library/hint.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Thu May 11 2017 01:45:10 GMT-0300 (BRT)
+ * Static Here - in Fri May 12 2017 14:54:01 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
