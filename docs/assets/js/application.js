455d24aec0b7b6c80a5891c8469f87979d49a190
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-12 20:43:45 -0300

    - table

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Fri May 12 2017 19:06:14 GMT-0300 (BRT)
+ * Static Here - in Fri May 12 2017 20:42:03 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
