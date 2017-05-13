43b7a9e9e4683283fbdbe11c10a9affd8bfc856a
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-13 12:36:13 -0300

    - text-center para a lista de documentacao

--- docs/assets/js/library/modal.js
+++ docs/assets/js/library/modal.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Sat May 13 2017 12:26:52 GMT-0300 (BRT)
+ * Static Here - in Sat May 13 2017 12:35:33 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
