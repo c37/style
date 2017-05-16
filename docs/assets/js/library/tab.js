d47eb852efa451dabfc43da9e17b22d1e9dc7b32
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-15 22:27:09 -0300

    - teste para mav-section

--- docs/assets/js/library/tab.js
+++ docs/assets/js/library/tab.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Mon May 15 2017 22:14:21 GMT-0300 (BRT)
+ * Static Here - in Mon May 15 2017 22:24:30 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
