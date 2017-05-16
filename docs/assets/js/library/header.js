baa860d311c895c469fa4ccc04e151f9491424cd
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-16 12:19:54 -0300

    - nav-menu + icon = melhorias

--- docs/assets/js/library/header.js
+++ docs/assets/js/library/header.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 16 2017 01:54:41 GMT-0300 (BRT)
+ * Static Here - in Tue May 16 2017 12:18:28 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
--- docs/assets/js/vendor/i18next.js
+++ docs/assets/js/vendor/i18next.js
@@ -1,6 +1,6 @@
 /**
  *
- * Static Here - in Tue May 16 2017 01:54:45 GMT-0300 (BRT)
+ * Static Here - in Tue May 16 2017 12:18:31 GMT-0300 (BRT)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
