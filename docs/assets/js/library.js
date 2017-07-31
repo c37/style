ebf98ab550d9b9331c49962c93fd99deb3d1e7b3
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-07-31 01:20:40 -0300

    - melhorando posicoes para componentes de Tab

--- docs/assets/js/library.js
+++ docs/assets/js/library.js
@@ -1,6 +1,6 @@
 /**
  *
- * C37 Style - in Sun Jul 23 2017 19:09:49 GMT-0300 (E. South America Standard Time)
+ * C37 Style - in Mon Jul 31 2017 01:19:19 GMT-0300 (E. South America Standard Time)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
