6a1d193ec3ade7bea562e469d951cfa38790599f
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-08-28 21:01:10 -0300

    - melhorando a class form, colocando as validacoes

--- docs/assets/js/library.js
+++ docs/assets/js/library.js
@@ -1,6 +1,6 @@
 /**
  *
- * C37 Style - in Mon Aug 14 2017 13:51:50 GMT-0300 (E. South America Standard Time)
+ * C37 Style - in Mon Aug 28 2017 20:58:34 GMT-0300 (E. South America Standard Time)
  *
  * c37-styleguide - styleguide of C37 - CNC
  * @version 0.0.1
